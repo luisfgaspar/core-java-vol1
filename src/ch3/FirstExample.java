44c6d83b0569026ef674646821c6400b3de5db85
Luís Gaspar <gasparfluis@gmail.com>
2019-01-18 00:20:05 -0200

    Comentários adicionados em FirstExample

--- src/ch3/FirstExample.java
+++ src/ch3/FirstExample.java
@@ -1,7 +1,22 @@
 package ch3;
 
+/**
+ * This is the first sample program in Core Java Chapter 3
+ * @version 1.01 1997-03-22
+ * @author Gary Cornel
+ */
+
+/* - public is called an access modifier;
+ * - everything lives and must be inside a class;
+ * - class names use CamelCase;
+ */
+
 public class FirstExample 
 {
+	/* - Java is case sensitive main != Main;
+	 * - Must have a main method to execute a code;
+	 * - The main method must be public;
+	 */
 	public static void main(String[] args) 
 	{
 		System.out.println("We will not use 'Hello World'");
